a2c1f66fb9ac0990bffe445ef2f3384dbbb13b96
mrc2rules <58372697+mrc2rules@users.noreply.github.com>
2022-05-03 19:55:12 +0300

    Update PastPaperOpener.bat

--- PastPaperOpener.bat
+++ PastPaperOpener.bat
@@ -1,13 +1,19 @@
 @echo off
 title IGCSE PastPaperOpener
 MODE CON:COLS=90 LINES=30
+set mode=View
+set vers=v1.2
+for /F %%I in ('curl -sS https://raw.githubusercontent.com/mrc2rules/IGCSE_PastPapers_Opener/main/version.txt') do set up=%%I
+if not %up%==%vers% set notif=Update Available!(%up%)
+if %up%==%vers% set notif=Up to date!
 :start
 echo ==========================================================================================
-echo                  Welcome! IGCSE PPO. Made by Rahbab Chowdhury 2022  
-echo.  
-echo.                                
-echo                        v1.1   [Options: Update, Dark/Light]      
-echo.                     [Changelog: Added abilty to change theme]   
+echo     Welcome! IGCSE PPO. Made by Rahbab Chowdhury 2022           =
+echo     %vers%   [Options: Update, Dark/Light]                        =
+echo.                                                                = Mode: %mode%ing Papers
+echo.    Type "Download" to download or "View" to view papers        =
+echo.                                                                =
+echo.    [Changelog: Added abilty to change theme]                   =  %notif%
 echo ==========================================================================================
 goto :menu
 :update
@@ -25,6 +31,8 @@ goto :start
 :menu
 set /p Subject=Enter subject code: 
 if /I "%Subject%" == "update" goto :update
+if /I "%Subject%" == "download" set mode=Download& cls & goto :start
+if /I "%Subject%" == "view" set mode=View& cls & goto :start
 if /I "%Subject%" == "LIGHT" set clr=F0 & goto :clr
 if /I "%Subject%" == "DARK" set clr=07 & goto :clr
 set /p Session=Enter letter(FM is m, MJ is s, ON is w): 
@@ -34,9 +42,21 @@ set /p Paper=Enter variant:
 echo.
 echo. The paper you have selected is %Subject%_%Session%%Year%_%Type%_%Paper%
 echo.
+if /I "%mode%" == "download" goto :download
 :confirm
-SET /P sure=Open Paper? (y/n):
+SET /P sure=%mode% Paper? (y/n):
 IF /I "%sure%" NEQ "y" goto :N
+:download
+cls
+echo Downloading...(Downloads to the directory where this program is located)
+echo.
+curl https://dynamicpapers.com/wp-content/uploads/2015/09/%Subject%_%Session%%Year%_%Type%_%Paper%.pdf --output %Subject%_%Session%%Year%_%Type%_%Paper%.pdf
+echo.
+echo Downloaded! Going back to start
+timeout /t 3
+cls
+goto :start
+timeout
 :open
 echo Opening...
 start https://dynamicpapers.com/wp-content/uploads/2015/09/%Subject%_%Session%%Year%_%Type%_%Paper%.pdf
