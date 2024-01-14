38fd11ac6d90c87ecb098d5f83fac7444327d5ed
Rahbab <58372697+mrc2rules@users.noreply.github.com>
2024-01-14 21:40:44 +0300

    Delete files directory

--- files/Run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-reg add HKCU\Console /v QuickEdit /t REG_DWORD /d 0 /f
-start "" "CAIE_PastPaperOpener"
--- files/msgbox.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:MsgBox prompt type title
-    setlocal enableextensions
-    set "tempFile=%temp%\%~nx0.%random%%random%%random%vbs.tmp"
-    >"%tempFile%" echo(WScript.Quit msgBox(Replace("%~1", "\n", vbCrLf),%~2,"%~3") & cscript //nologo //e:vbscript "%tempFile%"
-    set "exitCode=%errorlevel%" & del "%tempFile%" >nul 2>nul
-    endlocal & exit /b %exitCode%
